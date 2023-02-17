e77e6504e98ea2252f18408082796298d864557b
hhc123 <hhc123@smartlinks.com.cn>
2023-02-17 11:27:49 +0800

    修改ExtendDataUtil

--- lion-interface-expose-starter/src/main/java/com/lion/dubbo/util/ExtendDataUtil.java
+++ lion-interface-expose-starter/src/main/java/com/lion/dubbo/util/ExtendDataUtil.java
@@ -2,23 +2,14 @@ package com.lion.dubbo.util;
 
 import com.lion.constant.DubboConstant;
 import org.apache.dubbo.rpc.Invocation;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.util.StringUtils;
-
-import java.util.Objects;
 
 public class ExtendDataUtil {
     public static void setExtendData(Invocation invocation){
         ClientRemoteAddressUtil.setClientRemoteAddress();
         Object username = invocation.getObjectAttachments().get(DubboConstant.USERNAME);
-        if (Objects.nonNull(username) && StringUtils.hasText((String) username)) {
-            CurrentUserUtil.setCurrentUser((String) username);
-        }
-
+        CurrentUserUtil.setCurrentUser((String) username);
         Object tenantId = invocation.getObjectAttachments().get(DubboConstant.TENANT_ID);
-        if (Objects.nonNull(tenantId)) {
-            CurrentTenantIdUtil.setTenantId((Long) tenantId);
-        }
+        CurrentTenantIdUtil.setTenantId((Long) tenantId);
 //        TarceIdUtil.setTarceId();
     }
 }
